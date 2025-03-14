7184d3651ae0c82e6cbe625646cc0d253f8c29ba
Abdosaber <115594395+Abdosaber123@users.noreply.github.com>
2025-03-14 22:43:12 +0200

    Update Header.jsx

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import img from "../assets/logo-BfNap0Pe.PNG";
+import img from "../assets/logo-BfNap0Pe.png";
 import { Link } from "react-router-dom";
 export default function Header() {
   return (
